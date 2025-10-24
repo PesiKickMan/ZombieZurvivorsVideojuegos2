89ba63d918937a8543ddbf52b0d8c3061537afe7
PesiKickMan <candia.matias2000@gmail.com>
2025-10-24 20:18:35 -0300

    Agregados Botones al menu

--- Assets/Scripts/Game Manager/TitleScreenManager.cs
+++ Assets/Scripts/Game Manager/TitleScreenManager.cs
@@ -1,13 +1,47 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class TitleScreenManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject controlsPanel;
+
+        void Start()
+    {
+        // Asegurarse de que el panel de controles esté oculto al inicio
+        if (controlsPanel != null)
+            controlsPanel.SetActive(false);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            SceneManager.LoadScene("Game");
+            StartGame();
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && controlsPanel != null && controlsPanel.activeSelf)
+        {
+            HideControlsMenu();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+        public void HideControlsMenu()
+    {
+        if (controlsPanel != null)
+            controlsPanel.SetActive(false);
+    }
+
+    public void ToggleControlsMenu()
+    {
+        if (controlsPanel != null)
+            controlsPanel.SetActive(!controlsPanel.activeSelf);
     }
 }
